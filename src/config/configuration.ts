a54f4c95ed621c2f114230b8b51b3c5b13fba905
dazes97 <danielzeballos97@gmail.com>
2023-08-26 10:45:10 -0400

    refactor:removed api token keys
    remove api tokens

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -2,7 +2,6 @@ const Configuration = () => ({
   port: parseInt(process.env.SERVER_PORT, 10) || 3000,
   github: {
     baseUrl: process.env.GITHUB_API_BASE_URL,
-    token: process.env.GITHUB_API_TOKEN,
   },
   origin: {
     url: process.env.ORIGIN_URL,
